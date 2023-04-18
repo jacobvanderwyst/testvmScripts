f69dac52572c35bdf98d9c2fd9b6b7685b51a322
Jacob Vander Wyst <vjake870@gmail.com>
2023-04-18 12:11:46 -0500

    do

--- updateSQLdb.py
+++ updateSQLdb.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 import mysql.connector
+import time
 
-dbConnect=mysql.connector.connect(
-    host='localhost', port='3306', user='root', password=''
+dbConnect=mysql.connector.connect( 
+    host='localhost', port='3306', user='root', password='', database='ranchersonly'
 )
-
 print(dbConnect)
 
 mycursor = dbConnect.cursor()
 
-mycursor.execute("SHOW DATABASES")
+while True:
+    do=input("Enter Value: quit 0, add user 1, add rancher 2, add staff 3, add relationship 4\n$ ")
+    if do =='0':
+        break
+    elif do =='1':
+        mycursor.execute()
+    elif do =='2':
+        mycursor.execute()
+    elif do =='3':
+        mycursor.execute()
+    elif do =='4':
+        mycursor.execute()
 
-for x in mycursor:
-  print(x)
+print("Exiting...")
+time.sleep(1)
